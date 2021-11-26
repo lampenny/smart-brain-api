c9adea4b699de83b1a2db47f91a7c0b44fcae6de
Penny Lam <pennylam@live.com>
2021-11-26 11:05:45 +0900

    add utils file

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-const express = require('express');
+const { knex, db, bcrypt } = require("./utils/admin");
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-
-const db = knex({
-  client: 'pg',
-  connectionString : process.env.DATABASE_URL,
-  ssl: true
-});
 
 const app = express();
 
